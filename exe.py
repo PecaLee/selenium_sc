37f8302a9943204350c992035bfecb34b57507de
PecaLee <58648931+PecaLee@users.noreply.github.com>
2020-03-30 16:20:57 +0900

    rename_main.py

--- exe.py
+++ exe.py
@@ -5,12 +5,11 @@ path = "..\chromedriver\chromedriver.exe"
 
 
 driver = webdriver.Chrome(path)
-driver.get("https://l0o02.github.io/2018/06/12/python-crawling-selenium-1/")
+driver.get("https://www.naver.com/")
     
-    
+
 req = driver.page_source
 soup = BeautifulSoup(req, "lxml")
-find_div = soup.find("div")
-find_span = find_div.find("span").text
+find_title = soup.find("title").text
 
-print(find_span)
+print(find_title)
